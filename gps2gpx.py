a3e55f1b66a6a34c59768181ed2faec7afb98075
Nizhi Saeba <mathereall@gmail.com>
2019-10-15 23:38:44 -0500

    Update gps2gpx.py

--- gps2gpx.py
+++ gps2gpx.py
@@ -4,9 +4,7 @@ from bs4 import BeautifulSoup
 from tqdm import tqdm
 import xml.dom.minidom
 
-args = sys.argv
-
-with open(args[1], encoding='utf-8') as data_file:
+with open(sys.argv[1], encoding='utf-8') as data_file:
     data = json.loads(data_file.read())
 
 soup = BeautifulSoup('<gpx version="1.1" creator="Nizhi Saeba" xmlns="http://www.topografix.com/GPX/1/1" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.topografix.com/GPX/1/1 http://www.topografix.com/GPX/1/1/gpx.xsd"><trk><trkseg></trkseg></trk></gpx>', features='xml')
@@ -15,7 +13,6 @@ puntos = data['extended_gps_info']
 print("\n\nConvirtiendo " + str(len(puntos)) + " puntos GPS a GPX\n")
 
 for i in tqdm(puntos):
-
     punto = soup.new_tag("trkpt", lat=i['latitude'], lon=i['longitude'])
     punto.append(soup.new_tag("ele"))
     punto.ele.append(str(i['altitude']))
@@ -25,10 +22,9 @@ for i in tqdm(puntos):
 
 print("Escribiendo GPX...")
 xml_str = xml.dom.minidom.parseString(str(soup))
-pretty_xml_as_string = xml_str.toprettyxml()
-pretty_xml_as_string += "\n"
+pretty_xml_as_string = xml_str.toprettyxml() += "\n"
 
-with open(args[1].split(".")[0] + ".gpx", "w") as f:
+with open(sys.argv[1].split(".")[0] + ".gpx", "w") as f:
     f.write(pretty_xml_as_string)
 
 print("Finalizado")
